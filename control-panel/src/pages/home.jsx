e5310a6f7a8ef866cf0eebf55c2384e046ff7973
Meemum <liamdjasher@icloud.com>
2025-08-16 09:32:14 +1000

    Add welcome message to home page

--- control-panel/src/pages/home.jsx
+++ control-panel/src/pages/home.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 const Home = () => {
   return (
-        <h1 class = "text-3xl font-bold underline"> Hi </h1>
+    <div class="w-screen h-screen justify-center items-center flex flex-col">  
 
+        <h1 class="text-3xl font-bold underline"> Welcome to Legionnaire </h1>
+        <p class="pt-5 text-2xl">This is the control panel.</p>
+        <p class="pt-2 text-xl">Use the navigation bar to access different pages.</p>
+        
+    </div>
+    
   );
 }
 export default Home;
