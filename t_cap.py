6275e212f96f2815dd224071b592ae5878295041
smp46 <me@smp46.me>
2025-08-16 09:07:39 +1000

    Can now send flow features to an API.

--- t_cap.py
+++ t_cap.py
@@ -10,31 +10,35 @@ OUTPUT_DIR = "csv_output/"
 
 
 def sniff(interface):
-    """ """
+    """
+    Starts a continuous network traffic analysis session on a given interface,
+    periodically flushing the captured flow data to a CSV file.
+    """
     print("Starting network traffic analysis...")
 
     sniffer, session = create_sniffer(
         input_file=None,
         input_interface=interface,
         output_mode="csv",
-        output=csv_output + interface + ".csv",
+        output=OUTPUT_DIR + interface + ".csv",
         verbose=False,
     )
 
     sniffer.start()
-
     print("Sniffer started, analyzing packets...")
 
     try:
-        sniffer.join()
-    except:
-        print("Stopping sniffer...")
+        while True:
+            time.sleep(30)
+            print("Flushing flows to file...")
+            session.garbage_collect(time.time())
+    except KeyboardInterrupt:
+        print("\nStopping sniffer...")
         sniffer.stop()
     finally:
-        if sniffer.running:
-            sniffer.join()
+        print("Flushing remaining flows...")
         session.flush_flows()
-        print("Analysis complete. Flows saved to flows.csv")
+        print(f"Analysis complete. Flows saved to {OUTPUT_DIR}{interface}.csv")
 
 
 def fetch_interfaces():
@@ -58,13 +62,24 @@ def send_csvs(interval=60):
     """
     current_path = os.path.dirname(os.path.realpath(__file__))
     while True:
-        send_files = [f for f in listdir(current_path) if isfile(join(current_path, f))]
+        send_files = [
+            f
+            for f in os.listdir(current_path + "/" + OUTPUT_DIR)
+            if os.path.isfile(os.path.join(current_path + "/" + OUTPUT_DIR, f))
+        ]
         for file in send_files:
             try:
-                response = requests.post(url, data={"file": open(file, "rb")})
+                response = requests.post(
+                    "http://127.0.0.1:5000",
+                    data={
+                        file: open(
+                            os.path.join(current_path + "/" + OUTPUT_DIR, file), "rb"
+                        )
+                    },
+                )
             except Exception as e:
                 print(f"Error sending file {file}: {e}")
-        sleep(interval)
+        time.sleep(5)
 
 
 def main():
@@ -74,6 +89,9 @@ def main():
         print(f"Sniffing on interface: {interface}")
         t = threading.Thread(target=sniff, args=(interface,))
         threads.append(t)
+
+    t = threading.Thread(target=send_csvs, args=(60,))
+    threads.append(t)
 
     for t in threads:
         t.start()
