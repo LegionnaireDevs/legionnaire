c62d2c0d5ffa1ac6e716e7cb29b61bc9314b0c6f
Brandon <118105318+Bangu7@users.noreply.github.com>
2025-08-16 23:05:14 +1000

    Cleaned up model_predict.py.

--- model/model_predict.py
+++ model/model_predict.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import xgboost as xg
 
-model = xg.XGBClassifier()
-model.load_model("model/models/run9_bi.ubj")
+MODEL_FP = "model/models/run9_bi.ubj
 
+model = xg.XGBClassifier()
+model.load_model(MODEL_FP)
 
 def predict(df):
     return pd.DataFrame({"Label": model.predict(df)})
