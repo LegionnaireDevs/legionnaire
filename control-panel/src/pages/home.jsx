8f96214731215a3173a007b0be5ca22aaf386abb
Lachlanoc <lachlanoco@gmail.com>
2025-08-16 14:30:08 +1000

    Introduced baisc StatsCard display title and value

--- control-panel/src/pages/home.jsx
+++ control-panel/src/pages/home.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React from "react";
+import StatCard from "../components/StatCard.jsx";
+
+function Stats({ children }) {
+  return <div className="">{children}</div>;
+}
+
 const Home = () => {
   return (
-    <div class="w-screen h-screen justify-center items-center flex flex-col">  
-
-        <h1 class="text-3xl font-bold underline"> Welcome to Legionnaire </h1>
-        <p class="pt-5 text-2xl">This is the control panel.</p>
-        <p class="pt-2 text-xl">Use the navigation bar to access different pages.</p>
-        
+    <div class="w-screen h-screen justify-center items-center flex flex-col">
+      <h1 class="text-3xl font-bold underline"> Welcome to Legionnaire </h1>
+      <p class="pt-5 text-2xl">This is the control panel.</p>
+      <p class="pt-2 text-xl">
+        Use the navigation bar to access different pages.
+      </p>
+      <div className="grid grid-flow-col grid-row-3 gap-1">
+        <Stats>
+          <StatCard title={"Active Clients"} value={"Hardcode"} />
+        </Stats>
+        <Stats>
+          <StatCard title={"Active Clients"} value={"47"} />
+        </Stats>
+        <Stats>
+          <StatCard title={"Active Clients"} value={"Hardcode"} />
+        </Stats>
+      </div>
     </div>
-    
   );
-}
+};
 export default Home;
