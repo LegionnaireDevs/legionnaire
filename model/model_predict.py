6047f6924163335e664f49e63c6908c1bfe7b9b6
Brandon <118105318+Bangu7@users.noreply.github.com>
2025-08-16 22:32:26 +1000

    Update model_predict.py to accept dataframe

--- model/model_predict.py
+++ model/model_predict.py
@@ -5,28 +5,5 @@ import matplotlib.pyplot as plt
 model = xg.XGBClassifier()
 model.load_model('model/models/run9_bi.ubj')
 
-def predict(data_fp):
-    data = pd.read_csv(data_fp)
-    df = pd.DataFrame(data)
-    # df = df.drop(columns=['BwdPacketLengthMax', 'BwdPacketLengthMin', 'PSHFlagCount',
-    #                     'TotalLengthofBwdPackets', 'PacketLengthMean', 'BwdPacketLengthMean',
-    #                     'SubflowBwdBytes', 'AvgBwdSegmentSize'])
+def predict(df):
     return pd.DataFrame({'Label': model.predict(df)})
-
-# feature_importances = model.feature_importances_
-# xg.plot_importance(model, importance_type='gain')
-# plt.show()
-
-# import seaborn as sns
-# import matplotlib.pyplot as plt
-
-# df = pd.DataFrame(pd.read_csv('model/data/processed/processed.csv'))
-# # sns.boxplot(x='Label', y='BwdPacketLengthMax', data=df)
-# # plt.show()
-# X = df.drop(columns=['Label'])
-# y = df['Label']
-# from sklearn.feature_selection import mutual_info_classif
-
-# mi_scores = mutual_info_classif(X, y)
-# feature_scores = pd.Series(mi_scores, index=X.columns).sort_values(ascending=False)
-# print(feature_scores.head(10))
