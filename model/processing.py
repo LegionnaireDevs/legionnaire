a9de783475ac6afd5dbe00e06eba64ca12f9c12b
Brandon <118105318+Bangu7@users.noreply.github.com>
2025-08-17 11:35:25 +1000

    Update processing.py to fit latest data used for training.

--- model/processing.py
+++ model/processing.py
@@ -5,20 +5,7 @@ from sklearn.preprocessing import OneHotEncoder
 TARGET = 'Label'
 OUTPUT_FP = 'model/data/processed/processed.csv'
 
-attack_encode = {'BENIGN': 0, 'DDoS': 1, 'PortScan': 1, 'Bot': 1, 'Infiltration': 1, 'WebAttackBruteForce': 1,
-                 'WebAttackXSS': 1, 'WebAttackSqlInjection': 1, 'FTP-Patator': 1, 'SSH-Patator': 1,
-                 'DoSslowloris': 1, 'DoSSlowhttptest': 1, 'DoSHulk': 1, 'DoSGoldenEye': 1,
-                 'Heartbleed': 1, 0: 0, 1: 1}
-
 data = []
-data.append(pd.read_csv("model/data/Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv"))
-data.append(pd.read_csv("model/data/Friday-WorkingHours-Afternoon-PortScan.pcap_ISCX.csv"))
-data.append(pd.read_csv("model/data/Friday-WorkingHours-Morning.pcap_ISCX.csv"))
-data.append(pd.read_csv("model/data/Thursday-WorkingHours-Afternoon-Infilteration.pcap_ISCX.csv"))
-data.append(pd.read_csv("model/data/Thursday-WorkingHours-Morning-WebAttacks.pcap_ISCX.csv"))
-data.append(pd.read_csv("model/data/Tuesday-WorkingHours.pcap_ISCX.csv"))
-data.append(pd.read_csv("model/data/Wednesday-workingHours.pcap_ISCX.csv"))
-
 real_attack = pd.read_csv("model/data/attack_converted.csv")
 real_attack['Label'] = 1
 real_plain = pd.read_csv("model/data/plain_converted.csv")
@@ -29,8 +16,6 @@ data.append(real_attack)
 df = pd.concat(data)
 
 print(df[TARGET].unique())
-
-df[TARGET] = df[TARGET].map(attack_encode)
 
 for col in df:
     if df[col].dtype == 'object':
