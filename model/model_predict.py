56281fe551df501e24698b38e4caa8de7c0670ff
Brandon <118105318+Bangu7@users.noreply.github.com>
2025-08-17 11:32:53 +1000

    Update model_predict.py

--- model/model_predict.py
+++ model/model_predict.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import xgboost as xg
 
-MODEL_FP = "model/models/run9_bi.ubj"
+MODEL_FP = "model/models/run13_bi.ubj"
 
 model = xg.XGBClassifier()
 model.load_model(MODEL_FP)
-
 
 def predict(df):
     return pd.DataFrame({"Label": model.predict(df)})
